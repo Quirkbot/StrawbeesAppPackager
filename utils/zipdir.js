effbc3be378c95970219efc34bb030781bc63968
Paulo Barcelos <paulo@azucrina.org>
2018-11-20 15:35:02 +0100

    Manually handling resource hacker

--- utils/zipdir.js
+++ utils/zipdir.js
@@ -4,17 +4,8 @@ const archiver = require('archiver')
 module.exports = async (src, dst, root) => new Promise((resolve, reject) => {
 	const output = fs.createWriteStream(dst)
 	const archive = archiver('zip')
-
-	output.on('close', function() {
-		console.log(archive.pointer() + ' total bytes')
-		console.log('archiver has been finalized and the output file descriptor has closed.')
-		resolve()
-	})
-
-	archive.on('error', function(err) {
-		reject(err)
-	})
-
+	output.on('close', () => resolve())
+	archive.on('error', (err) => reject(err))
 	archive.pipe(output)
 	archive.directory(src, root)
 	archive.finalize()
